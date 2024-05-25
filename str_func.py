1327d9f7fe3953802be1d5c0bcff33a17bbdf9b0
Dimanskr <dimanskr2@gmail.com>
2024-05-25 18:00:44 +0300

    add title function and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,17 @@
 def upper_str(string: str) -> str:
+    """
+    делаем все буквы в строке заглавными
+    :param string:
+    :return string:
+    """
     return string.upper()
+
+
+def title_str(string: str) -> str:
+    """
+    делаем каждую букву в строке заглавной
+    :param string:
+    :return string:
+    """
+    return string.title()
+
